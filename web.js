bf39d19264d324c442bfabcd56ad305e12f62281
Shreya-s23 <shreyasrinivas2005@gmail.com>
2024-08-08 09:20:12 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Topper", "Class Representative","Frontend developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
